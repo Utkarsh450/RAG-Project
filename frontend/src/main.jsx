b26be426c686f275c33bd05d5fef9a1ccb1ebd89
Utkarsh450 <ubarnwal0802@gmail.com>
2026-06-05 17:48:58 +0530

    main is fixed now

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './Context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
